ff0ab0b762d020ce3dc132d20e7ba9c6f5cee27d
Johann Petrak <johann.petrak@gmail.com>
2019-06-10 12:31:03 +0100

    Scaffold for example python program

---
+++ examples/tok1_program.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Simple demonstration for a program that tokenises the text it gets from the annotator framework
+and returns the annotations as JSON.
+"""
+
+def tokenise(text):
+    pass
+
+if __name__ == "__main__":
+    pass
